514300800388e7f4b298d148ff5405d574d6c15e
kgjacobsen89 <kgjacobsen89@gmail.com>
2014-09-09 12:16:41 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FlashLife::Application.config.secret_key_base = 'b0fbba65fa6b78f34a13d84f2580bae58acedd7c0035a2793f168c180e28c08dbe8f2654b0f9441185afb7a9e4c0049c31f1fafff3aca94c08aa122d93dcf6cf'
